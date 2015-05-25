a5700c8f7911982ac9814eff9037865c3ffb7c8d
Salvo Cirinà <salvocia90@gmail.com>
2015-05-25 18:23:33 +0200

    finita esercitazione

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import it.uniroma1.lcl.babelnet.BabelNet;
 import it.uniroma1.lcl.babelnet.BabelNetConfiguration;
 import it.uniroma1.lcl.babelnet.BabelSense;
 import it.uniroma1.lcl.babelnet.BabelSynset;
+import it.uniroma1.lcl.babelnet.BabelSynsetID;
+import it.uniroma1.lcl.babelnet.BabelSynsetIDRelation;
 import it.uniroma1.lcl.babelnet.data.BabelGloss;
+import it.uniroma1.lcl.babelnet.data.BabelPointer;
 import it.uniroma1.lcl.jlt.util.Language;
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,36 +26,39 @@ import java.util.Set;
 public class Main {
 
     static HashMap <String, String> lemmaMap;
+    static BabelNet bn;
     
     public static void main(String[] args) {
         Main m = new Main();
-        
+        bn = BabelNet.getInstance();
         lemmaMap = getLemmaMap();
         //Variabile che conterrá le configurazioni base di Babelnet
         BabelNetConfiguration conf = BabelNetConfiguration.getInstance();
         conf.setConfigurationFile(new File("config/babelnet.properties"));
 
-        BabelNet bn = BabelNet.getInstance();
+ 
 //        Properties props = new Properties();
 //        props.put("annotators", "tokenize, ssplit, pos, lemma");
 //        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
         //array di contesti per la parola pianta e testa
         String[] contestiPianta = {
-            "La pianta dell ' alloggio è disponibile in portineria ; dal disegno è possibile cogliere i dettagli dell ' architettura dello stabile , la distribuzione dei vani e la disposizione di porte e finestre .",
-            "I platani sono piante ad alto fusto , organismi viventi : non ha senso toglierli per fare posto a un parcheggio ."};
+            "La pianta dell'alloggio è disponibile in ufficio, accanto all'appartamento; dal disegno è possibile cogliere i dettagli dell'architettura dello stabile, sulla distribuzione dei vani e la disposizione di porte e finestre.",
+            "I platani sono piante ad alto fusto, organismi viventi: non ha senso toglierli per fare posto a un parcheggio."};
         String[] contestiTesta = {
-            "Si tratta di un uomo facilmente riconoscibile : ha una testa piccola , gli occhi sporgenti , naso adunco e piccole orecchie a sventola .",
-            "Come per tutte le cose , ci vorrebbe un po ' di testa , un po ' di ragione , cervello , per decidere con il bene dell ' intelletto ."};
-
+            "Si tratta di un uomo facilmente riconoscibile: ha una testa piccola, gli occhi sporgenti, naso adunco e piccole orecchie a sventola.",
+            "Come per tutte le cose, ci vorrebbe un po' di testa, un minimo di ragione, una punta di cervello, per non prendere decisioni fuori dal senso dell'intelletto."};
+        
         String[] words = {"pianta", "testa"};
-
+        
+        
         //aggiunta dei contesti pianta nella lista principale
         ArrayList<String> lista_contesti_pianta = new ArrayList<String>();
         lista_contesti_pianta.addAll(Arrays.asList(contestiPianta));
          //aggiunta dei contesti testa nella lista principale
         ArrayList<String> lista_contesti_testa = new ArrayList<String>();
         lista_contesti_testa.addAll(Arrays.asList(contestiTesta));
+        
         
         //rimuoviamo le stopwords a contesti pianta
         ArrayList<ArrayList<String>> contesti_pianta_noStopwords = new ArrayList<ArrayList<String>>();
@@ -73,9 +79,101 @@ public class Main {
         
         ArrayList<ArrayList<String>> lista_lemmi_contesti_testa = getLemsFromContexts(contesti_testa_noStopwords);
 //        System.out.println(lista_lemmi_contesti_testa);
+        
+        
+        ArrayList<BabelSynset> lista_synset_parole = getSynsetsFromWord(words[0]);
+        
+        BabelSynsetID id;
+        
+        for(int i = 0; i < contestiPianta.length; i++)
+        {
+            int max_overlap = 0;
+            int index = 0;
+            for(int j = 0; j < lista_synset_parole.size(); j++)
+            {
+                int overlap = 0;
+                ArrayList<BabelGloss> glosse_parola = (ArrayList<BabelGloss>) lista_synset_parole.get(j).getGlosses(Language.IT);
+                ArrayList<BabelSynsetIDRelation> lista_relazioni = ( ArrayList<BabelSynsetIDRelation>) lista_synset_parole.get(j).getEdges(BabelPointer.HYPERNYM);
+                for(int k = 0; k < lista_relazioni.size(); k++)
+                {
+                    id = lista_relazioni.get(k).getBabelSynsetIDTarget();
+                    glosse_parola.addAll(bn.getSynset(id).getGlosses(Language.IT));
+                }
+                ArrayList<String> lista_string_glossa = new ArrayList<>();
+                ArrayList<ArrayList<String>> lista_glosse_noStopword = new ArrayList<>();
+                for(int z = 0; z < glosse_parola.size(); z++)
+                {
+                    lista_string_glossa.add(glosse_parola.get(z).getGloss());
+                }
+                lista_glosse_noStopword = removeStopwordsFromList(lista_string_glossa);
+                lista_glosse_noStopword = getLemsFromContexts(lista_glosse_noStopword);   
+                for(int x = 0; x < lista_glosse_noStopword.size(); x++)
+                {
+                    overlap += computer_overlap(lista_lemmi_contesti_pianta.get(i), lista_glosse_noStopword.get(x));
+                }
+                if (overlap > max_overlap) {
+                    max_overlap = overlap;
+                    index = j;
+                }
+            }   
+            System.out.println(words[0] + " in the context '" + contestiPianta[i]
+                    + "' means '" + lista_synset_parole.get(index) + "' thanks to an overlap equals"
+                    + " to " + max_overlap);
+        }
+
+        lista_synset_parole = getSynsetsFromWord(words[1]);
+        
+        for(int i = 0; i < contestiTesta.length; i++)
+        {
+            int max_overlap = 0;
+            int index = 0;
+            for(int j = 0; j < lista_synset_parole.size(); j++)
+            {
+                int overlap = 0;
+                ArrayList<BabelGloss> glosse_parola = (ArrayList<BabelGloss>) lista_synset_parole.get(j).getGlosses(Language.IT);
+                ArrayList<BabelSynsetIDRelation> lista_relazioni = ( ArrayList<BabelSynsetIDRelation>) lista_synset_parole.get(j).getEdges(BabelPointer.HYPERNYM);
+                for(int k = 0; k < lista_relazioni.size(); k++)
+                {
+                    id = lista_relazioni.get(k).getBabelSynsetIDTarget();
+                    glosse_parola.addAll(bn.getSynset(id).getGlosses(Language.IT));
+                }
+                ArrayList<String> lista_string_glossa = new ArrayList<>();
+                ArrayList<ArrayList<String>> lista_glosse_noStopword = new ArrayList<>();
+                for(int z = 0; z < glosse_parola.size(); z++)
+                {
+                    lista_string_glossa.add(glosse_parola.get(z).getGloss());
+                }
+                lista_glosse_noStopword = removeStopwordsFromList(lista_string_glossa);
+                lista_glosse_noStopword = getLemsFromContexts(lista_glosse_noStopword);   
+                for(int x = 0; x < lista_glosse_noStopword.size(); x++)
+                {
+                    overlap += computer_overlap(lista_lemmi_contesti_testa.get(i), lista_glosse_noStopword.get(x));
+                }
+                if (overlap > max_overlap) {
+                    max_overlap = overlap;
+                    index = j;
+                }
+            }   
+            System.out.println(words[1] + " in the context '" + contestiTesta[i]
+                    + "' means '" + lista_synset_parole.get(index) + "' thanks to an overlap equals"
+                    + " to " + max_overlap);
+        }
 
     }
-
+    
+     private static int computer_overlap(ArrayList<String> context,
+            ArrayList<String> gloss) {
+        int count = 0;
+        for (String token_context : context) {
+            for (String token_gloss : gloss) {
+                if (token_context.equalsIgnoreCase(token_gloss)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
     public static Set<String> getStopwords() {
         Set<String> stopWords = new LinkedHashSet<String>();
         BufferedReader SW;
@@ -108,12 +206,24 @@ public class Main {
         Set<String> stopwords = getStopwords();
         String[] words = frase.split("[\\s]+");
         for (String word : words) {
-            if (!stopwords.contains(word.toLowerCase())) {
-                array_tokens.add(word);
+            String clean_word = word.replaceAll("[ \\p{Punct}]", " ");
+            String[] tokens = clean_word.split("[\\s]+");
+            for(String token : tokens)
+            {
+                if (!stopwords.contains(token.toLowerCase()))
+                    array_tokens.add(token);
             }
         }
-
         return array_tokens;
+    }
+    
+    public static ArrayList<ArrayList<String>> removeStopwordsFromList(ArrayList<String> lista){
+        ArrayList<ArrayList<String>> lista_stopwords_rimosse = new ArrayList<>();
+        for(String word : lista)
+        {
+            lista_stopwords_rimosse.add(removeStopwordsFromContexts(word));
+        }
+        return lista_stopwords_rimosse;
     }
     
     public static HashMap<String,String> getLemmaMap() {
@@ -155,6 +265,16 @@ public class Main {
             lista_lemmi_contesti.add(lemmi_contesto);
         }
         return lista_lemmi_contesti;
+    }
+    
+    public static ArrayList<BabelSynset> getSynsetsFromWord(String word){
+        
+        
+        ArrayList<BabelSynset> lista_Synset;
+        lista_Synset = (ArrayList<BabelSynset>) bn.getSynsets(Language.IT , word);
+
+        
+        return lista_Synset;
     }
 //  
 //    public ArrayList<ArrayList<String>> getLemsFromContexts(ArrayList<String> contexts, StanfordCoreNLP pipeline) {
